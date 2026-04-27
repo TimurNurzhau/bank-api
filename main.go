8643d0610e0a78ed449dd830f6286135eda2889f
TimurNurzhau <ali.zhau.new@gmail.com>
2026-04-27 19:57:24 +0300

    project structure and database connection

--- main.go
+++ main.go
@@ -1,7 +1,43 @@
 package main
 
-import "fmt"
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"os"
+
+	_ "github.com/lib/pq"
+)
 
 func main() {
-    fmt.Println("Go работает в VS Code!")
+	dbHost := getEnv("DB_HOST", "localhost")
+	dbPort := getEnv("DB_PORT", "5432")
+	dbUser := getEnv("DB_USER", "bankuser")
+	dbPassword := getEnv("DB_PASSWORD", "bankuser_pass_2024")
+	dbName := getEnv("DB_NAME", "bankapi")
+	dbSSLMode := getEnv("DB_SSLMODE", "disable")
+
+	dsn := fmt.Sprintf(
+		"host=%s port=%s user=%s password=%s dbname=%s sslmode=%s",
+		dbHost, dbPort, dbUser, dbPassword, dbName, dbSSLMode,
+	)
+
+	db, err := sql.Open("postgres", dsn)
+	if err != nil {
+		log.Fatalf("Failed to open database: %v", err)
+	}
+	defer db.Close()
+
+	if err := db.Ping(); err != nil {
+		log.Fatalf("Failed to ping database: %v", err)
+	}
+
+	fmt.Println("Successfully connected to PostgreSQL!")
+}
+
+func getEnv(key, defaultVal string) string {
+	if val := os.Getenv(key); val != "" {
+		return val
+	}
+	return defaultVal
 }
